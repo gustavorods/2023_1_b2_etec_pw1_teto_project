046c6a74eee2bbb838631d504366e5990a316ca1
Gustavo Rodrigues <gustavorodrigues@pop-os.localdomain>
2023-05-11 02:34:15 -0300

    começo do music

--- home/teto-blog/resources/JavaScript/script.js
+++ home/teto-blog/resources/JavaScript/script.js
@@ -1,6 +1,12 @@
+//home
 function ola(nome) {
     var name;
     name = nome.value
 
-    alert(`OK ${name}, anotamos seu nome`)
+    alert(`OK ${name}, anotamos seu nome!`)
+}
+
+//musicas
+function music() {
+    alert(`Vamos ver as musicas dele!`)
 }
